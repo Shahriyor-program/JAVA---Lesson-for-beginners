7186d00a42a213a9608125304f3444754b751e96
PROGRAMs TJK <shhrryunusov@gmail.com>
2022-10-08 21:54:37 +0500

    My code for lesson for beginners

--- src/Lesson_5.java
+++ src/Lesson_5.java
@@ -10,7 +10,20 @@ public class Lesson_5 {
 //        System.out.println("Салом, " + user_name); //пешниҳод кардани маълумоти воридкардашуда барои истифодабаранда
 
         // Гирифтани маълумотҳои рақамӣ аз истифодабаранда:
-        int num1 = scanner.nextInt();   //дар ин ҳолат танҳо маълумотҳои рақами ворид карда мешаванд ҳангоми ворид кардани маълумотҳои сатри ба хатоги дучор мешавем
+//        int num1 = scanner.nextInt();   //дар ин ҳолат танҳо маълумотҳои рақами ворид карда мешаванд ҳангоми ворид кардани маълумотҳои сатри ба хатоги дучор мешавем
+
+        // Калкулятори одди:
+        System.out.print("Рақами якумро ворид кунед: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Рақами дуюмро ворид кунед: ");
+        int num2 = scanner.nextInt();
+
+        int res1 = num1 + num2;
+        int res2 = num1 - num2;
+        int res3 = num1 * num2;
+        int res4 = num1 / num2;
+
+        System.out.print(res1 + "\n" + res2 + "\n" + res3 + "\n" + res4);
 
     }
 }
