d80d42480182350e03b928b54d1afe41ad1b030d
PROGRAMs TJK <shhrryunusov@gmail.com>
2022-10-08 21:40:40 +0500

    My code for lesson for beginners

---
+++ src/Lesson_4.java
@@ -0,0 +1,9 @@
+public class Lesson_4 {
+    public static void main(String[] args) {
+        System.out.println("Дарси 3: Тағйирёбандаҳо");
+
+        boolean isHeppy = false;
+        int  x = 50;
+        int y = 60;
+    }
+}
---
+++ src/Lesson_5.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Lesson_5 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in); // .in гирфтани маълумот аз истифодабаранда
+//        scanner.nextLine(); // дархост кардани маълумот аз истифодабаранда
+
+//        System.out.print("Номи худро ворид кунед: "); // маълумот барои истифодабаранда
+//        String user_name = scanner.nextLine(); //дархости маълумот аз истифодабаранда
+//        System.out.println("Салом, " + user_name); //пешниҳод кардани маълумоти воридкардашуда барои истифодабаранда
+
+        // Гирифтани маълумотҳои рақамӣ аз истифодабаранда:
+        int num1 = scanner.nextInt();   //дар ин ҳолат танҳо маълумотҳои рақами ворид карда мешаванд ҳангоми ворид кардани маълумотҳои сатри ба хатоги дучор мешавем
+
+    }
+}
